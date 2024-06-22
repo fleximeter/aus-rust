33403807b9422e4783f284c7593aedad38167da5
Jeff Martin <jeffreymartin@outlook.com>
2024-06-21 19:42:20 -0500

    first version of analysis toolkit complete

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,9 +1,85 @@
 // File: analysis.rs
 // This file contains functionality for analyzing audio.
 
+use crate::spectrum;
+const FFT_SIZE: usize = 2048;
+
+pub struct Analysis {
+    pub dbfs: f64,
+    pub energy: f64,
+    pub spectral_centroid: f64,
+    pub spectral_variance: f64,
+    pub spectral_skewness: f64,
+    pub spectral_kurtosis: f64,
+    pub spectral_entropy: f64,
+    pub spectral_flatness: f64,
+    pub spectral_roll_off_50: f64,
+    pub spectral_roll_off_75: f64,
+    pub spectral_roll_off_90: f64,
+    pub spectral_roll_off_95: f64,
+    pub spectral_slope: f64,
+    pub spectral_slope_0_1_khz: f64,
+    pub spectral_slope_1_5_khz: f64,
+    pub spectral_slope_0_5_khz: f64,
+    pub zero_crossing_rate: f64,
+}
+
+/// Performs a suite of analysis tools on provided audio and returns an 
+/// Analysis object with the results
+pub fn analyzer(audio: &mut Vec<Vec<f64>>, sample_rate: u16) -> Analysis {
+    let mut analysis: Analysis;
+    let (magnitude_spectrum, _) = spectrum::complex_to_polar_rfft(spectrum::rfft(&mut audio[0], FFT_SIZE));
+    let power_spectrum = make_power_spectrum(&magnitude_spectrum);
+    let magnitude_spectrum_sum = magnitude_spectrum.iter().sum();
+    let power_spectrum_sum = power_spectrum.iter().sum();
+    let spectrum_pmf = spectrum_pmf(&power_spectrum, power_spectrum_sum);
+    let rfft_freqs = spectrum::rfftfreq(FFT_SIZE, sample_rate);
+
+    analysis.dbfs = dbfs_max(&audio[0]);
+    analysis.energy = energy(audio);
+    analysis.spectral_centroid = spectral_centroid(&magnitude_spectrum, &rfft_freqs, magnitude_spectrum_sum);
+    analysis.spectral_variance = spectral_variance(&spectrum_pmf, &rfft_freqs, analysis.spectral_centroid);
+    analysis.spectral_skewness = spectral_skewness(&spectrum_pmf, &rfft_freqs, analysis.spectral_centroid, analysis.spectral_variance);
+    analysis.spectral_kurtosis = spectral_kurtosis(&spectrum_pmf, &rfft_freqs, analysis.spectral_centroid, analysis.spectral_variance);
+    analysis.spectral_entropy = spectral_entropy(&spectrum_pmf);
+    analysis.spectral_flatness = spectral_flatness(&magnitude_spectrum, magnitude_spectrum_sum);
+    analysis.spectral_roll_off_50 = spectral_roll_off_point(&power_spectrum, &rfft_freqs, power_spectrum_sum, 0.5);
+    analysis.spectral_roll_off_75 = spectral_roll_off_point(&power_spectrum, &rfft_freqs, power_spectrum_sum, 0.75);
+    analysis.spectral_roll_off_90 = spectral_roll_off_point(&power_spectrum, &rfft_freqs, power_spectrum_sum, 0.9);
+    analysis.spectral_roll_off_95 = spectral_roll_off_point(&power_spectrum, &rfft_freqs, power_spectrum_sum, 0.95);
+    analysis.spectral_slope = spectral_slope(&power_spectrum, power_spectrum_sum);
+    analysis.spectral_slope_0_1_khz = spectral_slope_region(&power_spectrum, power_spectrum_sum, &rfft_freqs, 0.0, 1000.0, sample_rate);
+    analysis.spectral_slope_0_1_khz = spectral_slope_region(&power_spectrum, power_spectrum_sum, &rfft_freqs, 1000.0, 5000.0, sample_rate);
+    analysis.spectral_slope_0_1_khz = spectral_slope_region(&power_spectrum, power_spectrum_sum, &rfft_freqs, 0.0, 5000.0, sample_rate);
+    analysis.zero_crossing_rate = zero_crossing_rate(&audio[0], sample_rate);
+    analysis
+}
+
+/// Simple dot product function, implemented for code readability rather than using zip(), etc.
+#[inline]
+fn dot_product(vec1: &[f64], vec2: &[f64]) -> f64 {
+    let mut sum = 0.0;
+    for i in 0..vec1.len() {
+        sum += vec1[i] * vec2[i];
+    }
+    sum
+}
+
+/// Calculates max dbfs
+fn dbfs_max(audio: &Vec<f64>) -> f64 {
+    let mut maxval = 0.0;
+    for i in 0..audio.len() {
+        let sample_abs = audio[i].abs();
+        if sample_abs > maxval {
+            maxval = sample_abs;
+        }
+    }
+    20.0 * maxval.log10()
+}
+
 /// Extracts the RMS energy of the signal
 /// Reference: Eyben, pp. 21-22
-pub fn energy(audio: &Vec<Vec<f64>>) -> f64 {
+fn energy(audio: &Vec<Vec<f64>>) -> f64 {
     let mut sumsquare: f64 = 0.0;
     for i in 0..audio.len() {
         for j in 0..audio[i].len() {
@@ -17,14 +93,23 @@ pub fn energy(audio: &Vec<Vec<f64>>) -> f64 {
     }
 }
 
+/// Creates a power spectrum based on a provided magnitude spectrum
+fn make_power_spectrum(magnitude_spectrum: &Vec<f64>) -> Vec<f64> {
+    let mut power_spec: Vec<f64> = vec![0.0; magnitude_spectrum.len()];
+    for i in 0..magnitude_spectrum.len() {
+        power_spec[i] = magnitude_spectrum[i].powf(2.0);
+    }
+    power_spec
+}
+
 /// Calculates the spectral centroid from provided magnitude spectrum.
 /// It requires the sum of the magnitude spectrum as a parameter, since
 /// this is a value that might be reused.
 /// Reference: Eyben, pp. 39-40
-pub fn spectral_centroid(magnitude_spectrum: &Vec<f64>, magnitude_freqs: &Vec<f64>, magnitude_spectrum_sum: f64) -> f64 {
+fn spectral_centroid(magnitude_spectrum: &Vec<f64>, rfft_freqs: &Vec<f64>, magnitude_spectrum_sum: f64) -> f64 {
     let mut sum: f64 = 0.0;
     for i in 0..magnitude_spectrum.len() {
-        sum += magnitude_spectrum[i] * magnitude_freqs[i];
+        sum += magnitude_spectrum[i] * rfft_freqs[i];
     }
     if magnitude_spectrum_sum == 0.0 {
         return 0.0;
@@ -36,7 +121,7 @@ pub fn spectral_centroid(magnitude_spectrum: &Vec<f64>, magnitude_freqs: &Vec<f6
 /// Calculates the spectral entropy.
 /// It requires the power spectrum mass function (PMF).
 /// Reference: Eyben, pp. 23, 40, 41
-pub fn spectral_entropy(spectrum_pmf: &Vec<f64>) -> f64 {
+fn spectral_entropy(spectrum_pmf: &Vec<f64>) -> f64 {
     let mut entropy: f64 = 0.0;
     for i in 0..spectrum_pmf.len() {
         entropy += spectrum_pmf[i] * spectrum_pmf[i].log2();
@@ -47,7 +132,7 @@ pub fn spectral_entropy(spectrum_pmf: &Vec<f64>) -> f64 {
 /// Calculates the spectral flatness.
 /// It requires the power spectrum mass function (PMF).
 /// Reference: Eyben, p. 39, https://en.wikipedia.org/wiki/Spectral_flatness
-pub fn spectral_flatness(magnitude_spectrum: &Vec<f64>, magnitude_spectrum_sum: f64) -> f64 {
+fn spectral_flatness(magnitude_spectrum: &Vec<f64>, magnitude_spectrum_sum: f64) -> f64 {
     let mut log_spectrum_sum: f64 = 0.0;
     for i in 0..magnitude_spectrum.len() {
         log_spectrum_sum += magnitude_spectrum[i].ln();
@@ -56,10 +141,134 @@ pub fn spectral_flatness(magnitude_spectrum: &Vec<f64>, magnitude_spectrum_sum: 
     f64::exp(log_spectrum_sum) * magnitude_spectrum.len() as f64 / magnitude_spectrum_sum
 }
 
+/// Calculates the spectral variance
+/// 
+/// Requires the spectrum power mass function (PMF), RFFT magnitude frequencies, and spectral centroid
+/// Reference: Eyben, pp. 23, 39-40
+fn spectral_variance(spectrum_pmf: &Vec<f64>, rfft_freqs: &Vec<f64>, spectral_centroid: f64) -> f64 {
+    let mut spectral_variance: f64 = 0.0;
+    for i in 0..spectrum_pmf.len() {
+        spectral_variance += f64::powf(rfft_freqs[i] - spectral_centroid, 2.0) * spectrum_pmf[i];
+    }
+    spectral_variance
+}
+
+/// Calculates the spectral skewness
+/// 
+/// Requires the spectrum power mass function (PMF), RFFT magnitude frequencies, and spectral centroid
+/// Reference: Eyben, pp. 23, 39-40
+fn spectral_skewness(spectrum_pmf: &Vec<f64>, rfft_freqs: &Vec<f64>, spectral_centroid: f64, spectral_variance: f64) -> f64 {
+    let mut spectral_skewness: f64 = 0.0;
+    for i in 0..spectrum_pmf.len() {
+        spectral_skewness += f64::powf(rfft_freqs[i] - spectral_centroid, 3.0) * spectrum_pmf[i];
+    }
+    spectral_skewness /= spectral_variance.powf(1.5);
+    spectral_skewness
+}
+
+/// Calculates the spectral kurtosis
+/// 
+/// Requires the spectrum power mass function (PMF), RFFT magnitude frequencies, and spectral centroid
+/// Reference: Eyben, pp. 23, 39-40
+fn spectral_kurtosis(spectrum_pmf: &Vec<f64>, rfft_freqs: &Vec<f64>, spectral_centroid: f64, spectral_variance: f64) -> f64 {
+    let mut spectral_kurtosis: f64 = 0.0;
+    for i in 0..spectrum_pmf.len() {
+        spectral_kurtosis += f64::powf(rfft_freqs[i] - spectral_centroid, 4.0) * spectrum_pmf[i];
+    }
+    spectral_kurtosis /= spectral_variance.powf(2.0);
+    spectral_kurtosis
+}
+
+/// Calculates the spectral roll off frequency from provided power spectrum
+/// The parameter n (0.0 <= n <= 1.00) indicates the roll-off point we wish to calculate 
+/// Reference: Eyben, p. 41
+fn spectral_roll_off_point(power_spectrum: &Vec<f64>, rfft_freqs: &Vec<f64>, power_spectrum_sum: f64, n: f64) -> f64 {
+    let mut i: i64 = -1;
+    let mut cumulative_energy = 0.0;
+    while cumulative_energy < n && i < rfft_freqs.len() as i64 - 1 {
+        i += 1;
+        cumulative_energy += power_spectrum[i as usize] / power_spectrum_sum;
+    }
+    rfft_freqs[i as usize]
+}
+
+/// Calculates the spectral slope from provided power spectrum.
+/// Reference: Eyben, pp. 35-38
+fn spectral_slope(power_spectrum: &Vec<f64>, power_spectrum_sum: f64) -> f64 {
+    let n = power_spectrum.len() as f64;
+
+    // power spectrum will be the Y vector; we need to make an X vector
+    let mut x: Vec<f64> = vec![0.0; power_spectrum.len()];
+    for i in 0..power_spectrum.len() {
+        x[i] = i as f64;
+    }
+    
+    // the sum of x, and sum of x^2
+    let sum_x = n * (n - 1.0) / 2.0;
+    let sum_x_2 = n * (n - 1.0) * (2.0 * n - 1.0) / 6.0;
+
+    let slope = (n * dot_product(&power_spectrum, &x) - sum_x * power_spectrum_sum) / (n * sum_x_2 - sum_x.powf(2.0));
+    slope
+}
+
+/// Calculates the spectral slope from provided power spectrum, between the frequencies
+/// specified. The frequencies specified do not have to correspond to exact bin indices.
+/// Reference: Eyben, pp. 35-38
+fn spectral_slope_region(power_spectrum: &Vec<f64>, power_spectrum_sum: f64, rfft_freqs: &Vec<f64>, f_lower: f64, f_upper: f64, sample_rate: u16) -> f64 {
+    let fundamental_freq = sample_rate as f64 / ((power_spectrum.len() - 1) as f64 * 2.0);
+    
+    // The approximate bin indices for the lower and upper frequencies specified
+    let m_fl = f_lower / fundamental_freq;
+    let m_fu = f_upper / fundamental_freq;
+    
+    let n = power_spectrum.len() as f64;
+    let m_fl_ceil = m_fl.ceil();
+    let m_fl_floor = m_fl.floor();
+    let m_fu_ceil = m_fu.ceil();
+    let m_fu_floor = m_fu.floor();
+    
+    // these complicated formulas come from Eyben, p.37. The idea
+    // is to use interpolation in case the lower and upper frequencies
+    // do not correspond to exact bin indices.
+    
+    // calculate the sum_x
+    let sum_x: f64 = f_lower + rfft_freqs[m_fl_ceil as usize..m_fu_floor as usize].iter().sum() as f64 + f_upper;
+
+    // calculate the sum_y
+    let sum_y = power_spectrum[m_fl_floor as usize] + (m_fl - m_fl_floor) * 
+        (power_spectrum[m_fl_ceil as usize] - power_spectrum[m_fl_floor as usize]) + 
+        power_spectrum[m_fl_ceil as usize..m_fu_floor as usize].iter().sum() + 
+        power_spectrum[m_fu_floor as usize] + (m_fu - m_fu_floor) * 
+        (power_spectrum[m_fu_ceil as usize] - power_spectrum[m_fu_floor as usize]);
+    
+    // calculate sum_x^2
+    let sum_x_2 = f_lower.powf(2.0) + 
+        dot_product(&rfft_freqs[m_fl_ceil as usize..m_fu_floor as usize], 
+            &rfft_freqs[m_fl_ceil as usize..m_fu_floor as usize]) + 
+        f_upper.powf(2.0);
+
+    // calculate sum_xy
+    let sum_xy = f_lower * (power_spectrum[m_fl_floor as usize] + (m_fl - m_fl_floor) * (power_spectrum[m_fl_ceil as usize] - power_spectrum[m_fl_floor as usize])) +
+        dot_product(&power_spectrum[m_fl_ceil as usize..m_fu_floor as usize], &rfft_freqs[m_fl_ceil as usize..m_fu_floor as usize]) +
+        f_upper * (power_spectrum[m_fu_floor as usize] + (m_fu - m_fu_floor) * (power_spectrum[m_fu_ceil as usize] - power_spectrum[m_fu_floor as usize]));
+
+    let slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x_2 - sum_x.powf(2.0));
+    slope
+}
+
+/// Generates the spectrum power mass function (PMF) based on provided power spectrum 
+/// and sum of power spectrum
+fn spectrum_pmf(power_spectrum: &Vec<f64>, power_spectrum_sum: f64) -> Vec<f64> {
+    let mut pmf_vector: Vec<f64> = vec![0.0; power_spectrum.len()];
+    for i in 0..power_spectrum.len() {
+        pmf_vector[i] = power_spectrum[i] / power_spectrum_sum;
+    }
+    pmf_vector
+}
 
 /// Calculates the zero crossing rate.
 /// Reference: Eyben, p. 20
-pub fn zero_crossing_rate(audio: &Vec<f64>, sample_rate: u16) -> f64 {
+fn zero_crossing_rate(audio: &[f64], sample_rate: u16) -> f64 {
     let mut num_zc: f64 = 0.0;
     for i in 1..audio.len() {
         if audio[i-1] * audio[i] < 0.0 {
--- src/spectrum.rs
+++ src/spectrum.rs
@@ -2,6 +2,14 @@
 // This file contains spectral resource functionality.
 
 use realfft::RealFftPlanner;
+use num::Complex;
+
+pub enum WindowType{
+    Bartlett,
+    Blackman,
+    Hanning,
+    Hamming
+}
 
 /// Creates a Bartlett window of size m
 pub fn bartlett(m: usize) -> Vec<f64>{
@@ -43,15 +51,36 @@ pub fn hamming(m: usize) -> Vec<f64>{
 /// Calculates the real FFT of a chunk of audio.
 /// 
 /// The input audio must be a 1D vector already of the appropriate size.
-/// This function will return the magnitude and phase spectrum.
-pub fn rfft(audio: &mut Vec<f64>, fft_size: usize) -> (Vec<f64>, Vec<f64>) {
+/// This function will return the complex spectrum.
+pub fn rfft(audio: &mut [f64], fft_size: usize) -> Vec<Complex<f64>> {
     let mut real_planner = RealFftPlanner::<f64>::new();
     let r2c = real_planner.plan_fft_forward(fft_size);
     let mut input = audio;
-    let mut spectrum = r2c.make_output_vec();
+    let mut spectrum: Vec<num::Complex<f64>> = r2c.make_output_vec();
     assert_eq!(input.len(), fft_size);
     assert_eq!(spectrum.len(), fft_size / 2 + 1);
     r2c.process(&mut input, &mut spectrum).unwrap();
+    spectrum
+}
+
+/// Calculates the real IFFT of an audio spectrum.
+/// 
+/// The input audio must be a 1D vector already of the appropriate size.
+/// This function will return the audio.
+pub fn irfft(spectrum: &mut [Complex<f64>], fft_size: usize) -> Vec<f64> {
+    let mut real_planner = RealFftPlanner::<f64>::new();
+    let c2r = real_planner.plan_fft_inverse(fft_size);
+    let mut input = spectrum;
+    let mut audio: Vec<f64> = c2r.make_output_vec();
+    assert_eq!(input.len(), fft_size / 2 + 1);
+    assert_eq!(audio.len(), fft_size);
+    c2r.process(&mut input, &mut audio).expect("Something went wrong in irfft");
+    audio
+}
+
+/// This function creates the magnitude and phase spectra from provided
+/// complex spectrum.
+pub fn complex_to_polar_rfft(spectrum: Vec<Complex<f64>>) -> (Vec<f64>, Vec<f64>) {
     let mut magnitude_spectrum = vec![0.0 as f64; spectrum.len()];
     let mut phase_spectrum = vec![0.0 as f64; spectrum.len()];
     for i in 0..spectrum.len() {
@@ -59,6 +88,52 @@ pub fn rfft(audio: &mut Vec<f64>, fft_size: usize) -> (Vec<f64>, Vec<f64>) {
         phase_spectrum[i] = f64::atan2(spectrum[i].im, spectrum[i].re);
     }
     (magnitude_spectrum, phase_spectrum)
+}
+
+/// This function creates the magnitude and phase spectra from provided
+/// complex spectrum.
+pub fn complex_to_polar_rstft(spectrum: Vec<Vec<Complex<f64>>>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
+    let mut magnitude_spectrum: Vec<Vec<f64>> = Vec::new();
+    let mut phase_spectrum: Vec<Vec<f64>> = Vec::new();
+    for i in 0..spectrum.len() {
+        let mut frame_magnitude_spectrum = vec![0.0 as f64; spectrum[i].len()];
+        let mut frame_phase_spectrum = vec![0.0 as f64; spectrum[i].len()];
+        for j in 0..spectrum[i].len() {
+            frame_magnitude_spectrum[j] = f64::sqrt(f64::powf(spectrum[i][j].re, 2.0) + f64::powf(spectrum[i][j].im, 2.0));
+            frame_phase_spectrum[j] = f64::atan2(spectrum[i][j].im, spectrum[i][j].re);    
+        }
+        magnitude_spectrum.push(frame_magnitude_spectrum);
+        phase_spectrum.push(frame_phase_spectrum);
+    }    
+    (magnitude_spectrum, phase_spectrum)
+}
+
+/// This function creates the complex spectrum from provided
+/// magnitude and phase spectra
+pub fn polar_to_complex_rfft(magnitude_spectrum: Vec<f64>, phase_spectrum: Vec<f64>) -> Vec<Complex<f64>> {
+    let mut spectrum = vec![num::complex::Complex::new(0.0, 0.0); magnitude_spectrum.len()];    
+    for i in 0..magnitude_spectrum.len() {
+        let real = f64::cos(phase_spectrum[i]) * magnitude_spectrum[i];
+        let imag = f64::sin(phase_spectrum[i]) * magnitude_spectrum[i];
+        spectrum[i] = num::complex::Complex::new(real, imag);
+    }
+    spectrum
+}
+
+/// This function creates the complex spectrum from provided
+/// magnitude and phase spectra
+pub fn polar_to_complex_rstft(magnitude_spectrum: Vec<Vec<f64>>, phase_spectrum: Vec<Vec<f64>>) -> Vec<Vec<Complex<f64>>> {
+    let mut spectrum: Vec<Vec<Complex<f64>>> = Vec::new();
+    for i in 0..magnitude_spectrum.len() {
+        let mut frame_spectrum = vec![num::complex::Complex::new(0.0, 0.0); magnitude_spectrum[i].len()];    
+        for j in 0..magnitude_spectrum[i].len() {
+            let real = f64::cos(phase_spectrum[i][j]) * magnitude_spectrum[i][j];
+            let imag = f64::sin(phase_spectrum[i][j]) * magnitude_spectrum[i][j];
+            frame_spectrum[i] = num::complex::Complex::new(real, imag);
+        }
+        spectrum.push(frame_spectrum);
+    }
+    spectrum
 }
 
 /// Gets the corresponding frequencies for rFFT data
@@ -75,19 +150,116 @@ pub fn rfftfreq(fft_size: usize, sample_rate: u16) -> Vec<f64> {
 /// 
 /// The input audio must be a 1D vector already of the appropriate size.
 /// This function will return the magnitude and phase spectrum.
-pub fn rstft(audio: &mut Vec<f64>, fft_size: usize, hop_size: usize) -> (Vec<f64>, Vec<f64>) {
+pub fn rstft(audio: &mut Vec<f64>, fft_size: usize, hop_size: usize, window: WindowType) -> Vec<Vec<Complex<f64>>> {
     let mut real_planner = RealFftPlanner::<f64>::new();
     let r2c = real_planner.plan_fft_forward(fft_size);
-    let mut input = audio;
-    let mut spectrum = r2c.make_output_vec();
-    assert_eq!(input.len(), fft_size);
-    assert_eq!(spectrum.len(), fft_size / 2 + 1);
-    r2c.process(&mut input, &mut spectrum).unwrap();
-    let mut magnitude_spectrum = vec![0.0 as f64; spectrum.len()];
-    let mut phase_spectrum = vec![0.0 as f64; spectrum.len()];
-    for i in 0..spectrum.len() {
-        magnitude_spectrum[i] = f64::sqrt(f64::powf(spectrum[i].re, 2.0) + f64::powf(spectrum[i].im, 2.0));
-        phase_spectrum[i] = f64::atan2(spectrum[i].im, spectrum[i].re);
+    let mut spectrogram: Vec<Vec<Complex<f64>>> = Vec::new();
+    
+    // Get the window
+    let window_samples = match &window {
+        WindowType::Bartlett => bartlett(fft_size),
+        WindowType::Blackman => blackman(fft_size),
+        WindowType::Hanning => hanning(fft_size),
+        WindowType::Hamming => hamming(fft_size),
+        other => hanning(fft_size),
+    };
+    
+    let mut hop_idx = 0;
+    let mut finished = false;
+    while !finished {
+        let start_idx = hop_idx * hop_size;
+        let end_idx = if hop_idx * hop_size + fft_size < audio.len() {
+            hop_idx * hop_size + fft_size
+        } else {
+            audio.len()
+        };
+        let num_zeros = if end_idx == audio.len() {
+            finished = true;
+            start_idx + fft_size - end_idx
+        } else {
+            0
+        };
+
+        // Apply the window to the frame of samples
+        // Smaller window for the last frame if necessary
+        let mut fft_input = if num_zeros > 0 {
+            let window_samples = match &window {
+                WindowType::Bartlett => bartlett(end_idx - start_idx),
+                WindowType::Blackman => blackman(end_idx - start_idx),
+                WindowType::Hanning => hanning(end_idx - start_idx),
+                WindowType::Hamming => hamming(end_idx - start_idx),
+                other => hanning(end_idx - start_idx),
+            };
+            let mut input = {
+                let mut audio_chunk = audio[start_idx..end_idx].to_vec();
+                for i in 0..audio_chunk.len() {
+                    audio_chunk[i] *= window_samples[i];
+                }
+                audio_chunk
+            };
+            input.extend(vec![0.0; num_zeros]);
+            input
+        } else {
+            let mut input = {
+                let mut audio_chunk = audio[start_idx..end_idx].to_vec();
+                for i in 0..audio_chunk.len() {
+                    audio_chunk[i] *= window_samples[i];
+                }
+                audio_chunk
+            };
+            input
+        };
+
+        // prepare the output complex vector and check that the sizes are correct
+        let mut spectrum = r2c.make_output_vec();
+        assert_eq!(fft_input.len(), fft_size);
+        assert_eq!(spectrum.len(), fft_size / 2 + 1);
+        
+        // process the FFT for this frame, and push it onto the output vector
+        r2c.process(&mut fft_input, &mut spectrum).unwrap();
+        spectrogram.push(spectrum);
+        hop_idx += 1;
     }
-    (magnitude_spectrum, phase_spectrum)
+    spectrogram
 }
+
+/*
+/// Calculates the inverse real STFT of a chunk of audio.
+/// 
+pub fn irstft(spectrogram: &mut Vec<Vec<Complex<f64>>>, fft_size: usize, hop_size: usize, window: WindowType) -> Vec<Vec<f64>> {
+    let mut real_planner = RealFftPlanner::<f64>::new();
+    let c2r = real_planner.plan_fft_inverse(fft_size);
+    let mut audio: Vec<Vec<f64>> = Vec::new();
+    
+    // Get the window
+    let window_samples = match &window {
+        WindowType::Bartlett => bartlett(fft_size),
+        WindowType::Blackman => blackman(fft_size),
+        WindowType::Hanning => hanning(fft_size),
+        WindowType::Hamming => hamming(fft_size),
+        other => hanning(fft_size),
+    };
+    
+    let mut hop_idx = 0;
+    let mut finished = false;
+    while !finished {
+        
+
+        
+
+        // prepare the output complex vector and check that the sizes are correct
+        let ifft_input = spectrogram[hop_idx];
+        let mut audio = c2r.make_output_vec();
+        assert_eq!(ifft_input.len(), fft_size / 2 + 1);
+        assert_eq!(audio.len(), fft_size);
+        
+        // process the FFT for this frame, and push it onto the output vector
+        c2r.process(&mut ifft_input, &mut audio).expect("Something went wrong in irfft");
+
+        
+        
+        //spectrogram.push(spectrum);
+        hop_idx += 1;
+    }
+    spectrogram
+}*/
