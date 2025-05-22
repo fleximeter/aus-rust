7e589d819ce027595936b2bc8f51ca8ec42b6984
Jeff Martin <jeffreymartin@outlook.com>
2025-05-22 11:42:16 -0500

    more work on mel dev and testing

--- src/analysis/mel.rs
+++ src/analysis/mel.rs
@@ -74,8 +74,6 @@ impl TriangleFilter {
                 filter.push(triangle.compute(fft_freqs[i + start_idx]));
             }
         }
-        //dbg!(start_idx, end_idx, triangle.x1, triangle.x2, triangle.x3, triangle.y1, triangle.y2);
-        //dbg!(&filter);
         TriangleFilter {
             start_idx: start_idx,
             end_idx: end_idx,
@@ -110,7 +108,7 @@ impl MelFilterbank {
     /// with `num_filters` filters. If `quantize` is true, then the filter points are quantized
     /// to the nearest values in the provided array of `fft_freqs`.
     /// If `normalize` is true, then librosa-style filter scaling is applied.
-    pub fn new(freq_low: f64, freq_high: f64, num_filters: usize, fft_freqs: &[f64], quantize: bool, normalize: bool) -> MelFilterbank {
+    pub fn new(freq_low: f64, freq_high: f64, num_filters: usize, fft_freqs: &[f64], normalize: bool) -> MelFilterbank {
         // Determine the filter points, including a start point for the first filter
         // and an end point for the last filter
         let arr_len = num_filters + 2;
@@ -126,17 +124,9 @@ impl MelFilterbank {
             let low_freq: f64;
             let mid_freq: f64;
             let high_freq: f64;
-
-            // We may want to quantize the filterbank X points to actual values in `fft_freqs`
-            if quantize {
-                low_freq = fft_freqs[util::ordered_search(&fft_freqs, freq_center_freqs[i-1]).unwrap()];
-                mid_freq = fft_freqs[util::ordered_search(&fft_freqs, freq_center_freqs[i]).unwrap()];
-                high_freq = fft_freqs[util::ordered_search(&fft_freqs, freq_center_freqs[i+1]).unwrap()];
-            } else {
-                low_freq = freq_center_freqs[i-1];
-                mid_freq = freq_center_freqs[i];
-                high_freq = freq_center_freqs[i+1];
-            }
+            low_freq = freq_center_freqs[i-1];
+            mid_freq = freq_center_freqs[i];
+            high_freq = freq_center_freqs[i+1];
 
             // Create the triangle computation function and the triangular filter
             let tri = Triangle::new(low_freq, mid_freq, high_freq, 0.0, 1.0);
@@ -167,7 +157,7 @@ impl MelFilterbank {
     /// let audio = aus::read("myfile.wav").unwrap();
     /// let audio_chunk = &audio.samples[0][..fft_size];
     /// let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-    /// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+    /// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true);
     /// let imaginary_spectrum = spectrum::rfft(&audio_chunk, fft_size);
     /// let (magnitude_spectrum, phase_spectrum) = spectrum::complex_to_polar_rfft(&imaginary_spectrum);
     /// let power_spectrum = analysis::make_power_spectrum(&magnitude_spectrum);
@@ -270,7 +260,7 @@ pub fn melscale(freqs: &[f64]) -> Vec<f64> {
 /// let fft_size = 2048;
 /// let audio = aus::read("myfile.wav").unwrap();
 /// let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true);
 /// let imaginary_spectrogram = spectrum::rstft(&audio.samples[0], fft_size, fft_size / 2, aus::WindowType::Hanning);
 /// let (magnitude_spectrogram, _) = spectrum::complex_to_polar_rstft(&imaginary_spectrogram);
 /// let power_spectrogram = analysis::make_power_spectrogram(&magnitude_spectrogram);
@@ -300,20 +290,20 @@ pub fn make_mel_spectrogram(spectrogram: &[Vec<f64>], filterbank: &MelFilterbank
 /// let fft_size = 2048;
 /// let audio = aus::read("myfile.wav").unwrap();
 /// let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true);
 /// let audio_chunk = &audio.samples[0][..fft_size];
 /// let imaginary_spectrum = spectrum::rfft(&audio_chunk, fft_size);
 /// let (magnitude_spectrum, phase_spectrum) = spectrum::complex_to_polar_rfft(&imaginary_spectrum);
 /// let power_spectrum = analysis::make_power_spectrum(&magnitude_spectrum);
 /// let mel_spectrum = mel_filterbank.filter(&power_spectrum);
-/// let log_spectrum: Vec<f64> = analysis::make_log_spectrum(&mel_spectrum, 10e-8);
+/// let log_spectrum: Vec<f64> = analysis::make_log_spectrum(&mel_spectrum, 10.0, 10e-8, Some(-80.0));
 /// let mfccs = analysis::mel::mfcc_spectrum(&log_spectrum, 2.0); // then use indices 11-15
 /// ```
 pub fn mfcc_spectrum(mel_spectrum: &[f64], lifter: f64) -> Vec<f64> {
-    let mut planner = DctPlanner::new();
+    let mut planner: DctPlanner<f64> = DctPlanner::new();
     let dct3 = planner.plan_dct2(mel_spectrum.len());
-    let mut mfccs: Vec<f64> = make_log_spectrum(mel_spectrum, 10e-8);
-    dct3.process_dct3(&mut mfccs);
+    let mut mfccs: Vec<f64> = make_log_spectrum(mel_spectrum, 10.0, -10e8, Some(-80.0));
+    dct3.process_dct2(&mut mfccs);
     
     // Perform "liftering"
     if lifter > 0.0 {
@@ -340,7 +330,7 @@ pub fn mfcc_spectrum(mel_spectrum: &[f64], lifter: f64) -> Vec<f64> {
 /// let fft_size = 2048;
 /// let audio = aus::read("myfile.wav").unwrap();
 /// let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+/// let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true);
 /// let imaginary_spectrogram = spectrum::rstft(&audio.samples[0], fft_size, fft_size / 2, aus::WindowType::Hanning);
 /// let (magnitude_spectrogram, _) = spectrum::complex_to_polar_rstft(&imaginary_spectrogram);
 /// let power_spectrogram = analysis::make_power_spectrogram(&magnitude_spectrogram);
@@ -349,13 +339,12 @@ pub fn mfcc_spectrum(mel_spectrum: &[f64], lifter: f64) -> Vec<f64> {
 /// ```
 pub fn mfcc_spectrogram(mel_spectrogram: &[Vec<f64>], lifter: f64) -> Vec<Vec<f64>> {
     let mut mfccs: Vec<Vec<f64>> = Vec::new();
-    if mel_spectrogram.len() > 0 {
-        let mut planner = DctPlanner::new();
-        let dct3 = planner.plan_dct2(mel_spectrogram[0].len());
-        
-        for spec in mel_spectrogram {
-            let mut mfccs_vec: Vec<f64> = make_log_spectrum(spec, 10e-8);
-            dct3.process_dct3(&mut mfccs_vec);
+    let mut planner = DctPlanner::new();
+    let dct2 = planner.plan_dct2(mel_spectrogram[0].len());
+    if mel_spectrogram.len() > 0 {        
+        for i in 0..mel_spectrogram.len() {
+            let mut mfccs_vec: Vec<f64> = make_log_spectrum(&mel_spectrogram[i], 10.0, -10e8, Some(-80.0));
+            dct2.process_dct2(&mut mfccs_vec);
             
             // Perform "liftering"
             if lifter > 0.0 {
@@ -458,7 +447,7 @@ mod tests {
         const LOW_FREQ: f64 = 0.0;
         const HIGH_FREQ: f64 = 11025.0;
         let rfreqs = crate::spectrum::rfftfreq(FFT_SIZE, SAMPLE_RATE);
-        let fb = MelFilterbank::new(LOW_FREQ, HIGH_FREQ, NUM_MELS, &rfreqs, false, true);
+        let fb = MelFilterbank::new(LOW_FREQ, HIGH_FREQ, NUM_MELS, &rfreqs, true);
         
         // A fake FFT power spectrum to test on
         let fake_fft_power_spectrum: Vec<f64> = vec![
@@ -493,7 +482,7 @@ mod tests {
         let fft_size = 2048;
         let audio = crate::read("myfile.wav").unwrap();
         let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-        let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+        let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, false);
         let imaginary_spectrogram = spectrum::rstft(&audio.samples[0], fft_size, fft_size / 2, crate::WindowType::Hanning);
         let (magnitude_spectrogram, _) = spectrum::complex_to_polar_rstft(&imaginary_spectrogram);
         let power_spectrogram = analysis::make_power_spectrogram(&magnitude_spectrogram);
@@ -506,11 +495,86 @@ mod tests {
         let fft_size = 2048;
         let audio = crate::read("myfile.wav").unwrap();
         let rfft_freqs = spectrum::rfftfreq(fft_size, audio.sample_rate);
-        let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, true, false);
+        let mel_filterbank = analysis::mel::MelFilterbank::new(20.0, 8000.0, 40, &rfft_freqs, false);
         let imaginary_spectrogram = spectrum::rstft(&audio.samples[0], fft_size, fft_size / 2, crate::WindowType::Hanning);
         let (magnitude_spectrogram, _) = spectrum::complex_to_polar_rstft(&imaginary_spectrogram);
         let power_spectrogram = analysis::make_power_spectrogram(&magnitude_spectrogram);
         let mel_spectrogram = analysis::mel::make_mel_spectrogram(&power_spectrogram, &mel_filterbank);
         let _ = analysis::mel::mfcc_spectrogram(&mel_spectrogram, 2.0); // then use indices 11-15
+    }
+
+    #[test]
+    fn test_mfccs() {
+        const EPSILON: f64 = 1e-4;
+        const FFT_SIZE: usize = 256;
+        const SAMPLE_RATE: u32 = 22050;
+        const NUM_MELS: usize = 20;
+        const LOW_FREQ: f64 = 0.0;
+        const HIGH_FREQ: f64 = 11025.0;
+        let rfreqs = crate::spectrum::rfftfreq(FFT_SIZE, SAMPLE_RATE);
+        let fb = MelFilterbank::new(LOW_FREQ, HIGH_FREQ, NUM_MELS, &rfreqs, true);
+        
+        // A fake FFT power spectrum to test on
+        let fake_fft_power_spectrum: Vec<f64> = vec![
+            1e-2, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            12e-5, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            3e-4, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            2e-3, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            43e-6, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            2e-3, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            6e-5, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            4e-3, 2e-5, 15e-4, 21e-5, 1e-4, 2e-5, 11e-5, 31e-4, 132e-9, 22e-3, 152e-3, 201e-5, 123e-5, 221e-6, 11e-5, 32e-4,
+            2e-5
+        ];
+        let mel_spec = fb.filter(&fake_fft_power_spectrum);
+        let mfccs = mfcc_spectrum(&mel_spec, 0.0);
+
+        // This literal MFCC vector was generated from a Python mockup.
+        // The MFCCs should match this to within constant scaling.
+        let python_output = vec![
+            -3.63056272e+03, -1.15575353e+02,  3.99920882e+00, -3.11952359e+01,
+            -7.03609930e+01, -7.31430390e+01, -2.76098946e+01,  6.03085626e+01,
+            9.21038790e+01,  2.40460139e+01,  2.64166007e+00,  3.33797262e+01,
+            -5.26270203e+00, -5.77346214e+01,  2.73941215e+00,  6.99495354e+01,
+            -3.44963034e-01, -1.82435303e+01,  2.38099452e+01, -2.41995494e+01];
+
+        for i in 0..mfccs.len() {
+            assert!(f64::abs(mfccs[i] * 2.0 - python_output[i]) < EPSILON);
+        }
+        
+    }
+
+    // This test compares regular rustdct output with a SciPy DCT-Type II output, with norm=None.
+    #[test]
+    fn test_dct() {
+        const EPSILON: f64 = 1e-6;
+        let mut planner: DctPlanner<f64> = rustdct::DctPlanner::new();
+        let dct = planner.plan_dct2(16);
+        let mut myvec = vec![0.1, -0.3, 0.43, -0.2, 0.1, 0.22, -0.432, -0.01, 0.1, -0.3, 0.43, 0.422, 0.1, -0.22, -0.43223, -0.231];
+        dct.process_dct2(&mut myvec);
+        
+        // scipy.fft.dct(input, type=2, norm=None)
+        let python_output = vec![
+            -0.44646, 0.88984127, -1.16548374, 2.46539874, -2.63440108, -0.2669338,
+            1.18680059, -1.30619017, 1.39332563, -0.61721852, -1.45113856, 1.15916989,
+            3.94025219, 1.42283727, 0.40242927, 0.82046698];
+        
+        // assert that SciPy output is approximately 2 times rustdct output
+        for i in 0..myvec.len() {
+            assert!(f64::abs(2.0 * myvec[i] - python_output[i]) < EPSILON);
+        }
+
+        // a second vector to test
+        let mut myvec2 = vec![-3002.48488, -40.384, -958.38506, -1002.4035506, -877.2312, -321.12101, -558.9853, -20.5983, -505.9681, -120.2489, -6590.843, -10.48729, -583.86948, -59.834972, -212.12, -89.348];
+        let python_output2 = vec![
+            -29908.6260852,   -3049.72757743,   1953.89015,    -15692.81401219,
+            3241.78874056,   5128.05286375, -15217.1865778,    -955.11239568,
+            3916.70892585, -15803.26515976,  -2549.46171482,   5938.48155781,
+            -13807.33352441,  -1109.48122982,   9696.12754217, -15191.45693698];
+        dct.process_dct2(&mut myvec2);
+
+        for i in 0..myvec2.len() {
+            assert!(f64::abs(2.0 * myvec2[i] - python_output2[i]) < EPSILON);
+        }
     }
 }
